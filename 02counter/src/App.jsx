bd9a15e84c9fb0c8c56ad399d180afd4ff846931
hackhub817 <piyushguptaji123@gmail.com>
2024-02-06 14:02:38 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setcounter] = useState(15);
+  const addValue=()=>{
+    if(counter<20)
+    {
+      counter=counter+1;
+    }
+    setcounter(counter);
+    
+  }
+  const subvalue=()=>
+  {
+    if(counter>0)
+    {
+    counter=counter-1;
+    }
+    setcounter(counter);
+  }
+  return (
+    <>
+     <h1> Counter React</h1>
+     <h2>Counter value:{counter}</h2>
+
+     <button onClick={addValue}>Add value</button>
+     <br/>
+     <button onClick={subvalue}>remove value</button>
+
+    </>
+  )
+}
+
+export default App
